2d6c25f09c17fec953d57a7bd2153afae29edb91
crashboom34 <piecolorama@gmail.com>
2025-08-24 16:55:11 +0200

    Use createRoot for React 18

--- app.js
+++ app.js
@@ -601,4 +601,8 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+}
